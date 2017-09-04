58d39da91d5ffd6a90d968b173591e703a7816c9
Boris Glavic <lordpretzel@gmail.com>
2017-09-04 14:07:32 -0500

    updated script

--- startNoteBook.sh
+++ startNoteBook.sh
@@ -8,4 +8,4 @@ fi
 echo "Start Postgres"
 sudo docker run -d --name notebpostgres --rm -p 5432:5432 --network mynbnetwork iitdbgroup/cs425
 echo "Start notebook server"
-sudo docker run --user root --rm --name=mynotebook -v "$(pwd)":/home/jovyan/ -p 0.0.0.0:8888:8888/tcp --network mynbnetwork -d iitdbgroup/sql_notebook start-notebook.sh --NotebookApp.token=''
+sudo docker run --user root --rm --name=mynotebook -v "$(pwd)":/home/jovyan/ -p 0.0.0.0:8888:8888/tcp --network mynbnetwork -d iitdbgroup/sql-notebook start-notebook.sh --NotebookApp.token=''
