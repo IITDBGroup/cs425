d37dd34604f11af7a9d4a244de567c884dfefc38
Boris Glavic <lordpretzel@gmail.com>
2022-10-17 09:57:40 -0500

    updated code examples

---
+++ programming_language_examples/Python/README.md
@@ -0,0 +1,21 @@
+# Setup of the Example Python Application
+
+This example python script was tested using python 3.10.2.
+
+## Setup
+
+You have to install the `pycopg2` module that is used to access a Postgres database from python before running the script. You can use the provided requirements file with pip (ideally in a virtual environment):
+
+~~~sh
+pip install -r ./requirements.txt
+~~~
+
+## Running the application
+
+~~~sh
+python 
+~~~
+
+# Further reading
+
+- `psycopg2` manual: [https://www.psycopg.org/docs/index.html](https://www.psycopg.org/docs/index.html)
--- programming_language_examples/Python/postgres_ex.py
+++ programming_language_examples/Python/postgres_ex.py
@@ -1,10 +1,18 @@
 import psycopg2
 
+connection = {
+    "dbname": 'university',
+    "user": 'postgres',
+    "host": '127.0.0.1',
+    "password": 'test',
+    "port": 5450
+    }
+
 # Create a connection
 try:
-    conn = psycopg2.connect("dbname='cs425' user='postgres' host='localhost' password='test'")
+    conn = psycopg2.connect(**connection)
 except:
-    print "I am unable to connect to the database"
+    print("I am unable to connect to the database")
 
 # Create a curson
 cur = conn.cursor()
@@ -13,27 +21,31 @@ cur = conn.cursor()
 try:
     cur.execute("SELECT name FROM student")
 except:
-    print "I can't SELECT from student"
+    print("I can't SELECT from student")
 
 # now let's fetch all the rows and print them
 rows = cur.fetchall()
-print "\nResults: \n"
+print("\nResults: \n")
 for row in rows:
-    print "   ", row
+    print(f"   {row}")
 
 
 # now a query with more result columns
 try:
     cur.execute("SELECT id, name, tot_cred FROM student ORDER BY name ASC")
 except:
-    print "I can't SELECT from student"
+    print("I can't SELECT from student")
 
 rows = cur.fetchall()
-print "\nResults: \n"
+print("\nResults: \n")
 for row in rows:
-    print "   ", row
-    print " or to access a particular column (2nd one):", row[1]
+    # Rows are encoded as tuples
+    print(f"{row}") 
+    print(f" or to access a particular column (2nd one): {row[1]}")
 
+# transactions are explicitely terminated by running con.rollback() or con.commit()
+
+    
 # close the connection
 cur.close()
 
