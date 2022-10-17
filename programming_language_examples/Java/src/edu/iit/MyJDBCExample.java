63172e4bf64110a5c26548757094faa711abe283
Boris Glavic <lordpretzel@gmail.com>
2022-10-17 11:46:50 -0500

    SQL exampels

--- programming_language_examples/Java/src/edu/iit/MyJDBCExample.java
+++ programming_language_examples/Java/src/edu/iit/MyJDBCExample.java
@@ -3,6 +3,7 @@ package edu.iit;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Connection;
@@ -17,6 +18,38 @@ public class MyJDBCExample {
 	public static final String JDBC_URL = "jdbc:postgresql://" + JDBC_HOST + ":" + JDBC_PORT + "/" + JDBC_DB;
 	public static final String DBUSER = "postgres";
 	public static final String DBPASSWD = "test";
+
+	// method for printing results for any query
+	public static void printQueryResults(Connection c, String sql) {
+		try {
+			Statement s = c.createStatement();
+			ResultSet r = s.executeQuery(sql);
+			ResultSetMetaData md = r.getMetaData();
+			int numCols = md.getColumnCount();
+
+			System.out.printf("================================================================================" +
+							  "\nQUERY: %s" +
+							  "\n================================================================================\n",
+							  sql);
+			while(r.next()) {
+				System.out.print("(");
+				for(int i = 1; i <= numCols; i++) {
+					System.out.printf("%s: %s%s",
+									  md.getColumnName(i),
+									  r.getString(i),
+									  i < numCols ? ", ": ""
+						);					
+				}
+				System.out.println(")");					
+			}
+			
+			r.close();
+			s.close();
+		}
+		catch (SQLException e) {
+			System.err.println("An error occurred: " + e.toString());			
+		}
+	}
 	
 	public static void main (String[] args) throws Exception {
 		try {
@@ -141,24 +174,21 @@ public class MyJDBCExample {
 			// SELECT * FROM instructor u name = 'X' OR true OR name = '';
 			while(r.next())
 			{
-				System.out.println("The total of number of instructors is: " + r.getString(1) + "," + r.getString("name"));
+				System.out.println("Another instructor: " + r.getString(1) + "," + r.getString("name"));
 			}
 			r.close();
 
 			// manipulate the database
 			name = "X'; DELETE FROM instructor; SELECT 'a";
-			r = s.executeQuery("SELECT * FROM instructor WHERE name = '" + name + "'");
+			s.execute("SELECT * FROM instructor WHERE name = '" + name + "'");
 			// SELECT * FROM instructor WHERE name = 'X'; DELETE FROM instructor;
 			r.close();
 
-			r = s.executeQuery("SELECT count(*) FROM instructor;");
-			while(r.next())
-			{
-				System.out.println("The total of number of instructors is: " + r.getString(1));
-			}			
-			r.close();			
-			
+			printQueryResults(c, "SELECT count(*) FROM instructor;");
 			c.rollback();
+
+			// print arbitrary query results
+			printQueryResults(c, "SELECT count(*) AS nums, dept_name FROM student GROUP BY dept_name;");
 			
             // close connection
 			s.close();
